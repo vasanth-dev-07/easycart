eae86fc5a542d6288bca6eb654709843c28e6e43
vasanth_dev_07 <vasanthkumarj.job@gmail.com>
2025-07-29 16:06:40 +0530

    removing mail details

--- easycart/settings.py
+++ easycart/settings.py
@@ -155,6 +155,4 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vasanthkumarj.job@gmail.com'
-EMAIL_HOST_PASSWORD = 'istg muuu ctxg blus'
 
